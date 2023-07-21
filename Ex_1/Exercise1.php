6725dd6606fd669d212c377e4c06a27162b774fc
Nani <anajava2323@gmail.com>
2023-07-21 19:17:39 +0200

    Corrección del ejercicio

--- Ex_1/Exercise1.php
+++ Ex_1/Exercise1.php
@@ -3,7 +3,7 @@
 
 $_SESSION["username"] = $_POST["username"];
 $_SESSION["email"] = $_POST["email"];
-$_SESSION["password"] = $_POST["password"];
+
 
 echo $_SESSION["username"].PHP_EOL;
 echo $_SESSION["email"] .PHP_EOL;
